ed8111add8b36f37abbcafb9cde81eb159e0b93e
koenig6 <35234340+koenig6@users.noreply.github.com>
2018-03-14 10:47:22 -0400

    weather 365 added

--- assignments/weathersite/js/town.js
+++ assignments/weathersite/js/town.js
@@ -1,68 +1,64 @@
 
-//reference parts of HTML in variables
-var franklin = document.querySelector('div.franklin');
-var greenville = document.querySelector('div.greenville');
-var springfield = document.querySelector('div.springfield');
+//make a container for my town data
+var section = document.querySelector('#home_two');
 
-//JSON url stored into variable
+// requesting town data
 var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-//create and open request
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-
-//get response and convert from JSON to Javascript
 request.responseType = 'json';
 request.send();
+request.onload = function() {
+    var towndata = request.response;
+    var towns = towndata['towns'];
 
-//waiting for response and dealing with it when it arrives
-request.onload = function () {
-    var cityInfo = request.response;
-    displayCity(cityInfo);
-}
-
-
-//create city information sections
-function displayCity(jsonObj) {
-    var city = jsonObj['towns'];
-
-    for (var i = 0; i < city.length; i++) {
+    for (var i = 0; i < towns.length; i++)
+    {
+        if (i == 2)
+        {
+            i++;
+        }
         var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
+        var myH3 = document.createElement('h3');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
         var myPara5 = document.createElement('p');
         var myList = document.createElement('ul');
+        var myImg = document.createElement('img');
 
-        myH2.textContent = city[i].name;
-        myPara1.textContent = 'Motto: ' + city[i].motto;
-        myPara2.textContent = 'Year Founded: ' + city[i].yearFounded;
-        myPara3.textContent = 'Current Population: ' + city[i].currentPopulation;
-        myPara4.textContent = 'Average Rainfall: ' + city[i].averageRainfall;
+        myH3.textContent = towns[i].name;
+        myPara1.textContent = 'Motto: ' + towns[i].motto;
+        myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        myPara3.textContent = 'Population: ' + towns[i].currentPopulation;
+        myPara4.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall + ' inches';
         myPara5.textContent = 'Events:';
+        myImg.setAttribute('src', '/images/' + towns[i].name + '.jpg');
+        myImg.setAttribute('alt',towns[i] + 'city picture');
 
-        var events = city[i].events;
-        for (var j = 0; j < events.length; j++) {
+        var townEvents = towns[i].events;
+        for (var j = 0; j < townEvents.length; j++) {
             var listItem = document.createElement('li');
-            listItem.textContent = events[j];
+            listItem.textContent = townEvents[j];
             myList.appendChild(listItem);
         }
 
-        myArticle.appendChild(myH2);
+        myArticle.appendChild(myH3);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
         myArticle.appendChild(myPara4);
         myArticle.appendChild(myPara5);
         myArticle.appendChild(myList);
-        if (city[i].name == "Franklin") {
-            franklin.appendChild(myArticle);
-        } else if (city[i].name == "Greenville") {
-            greenville.appendChild(myArticle);
-        } else if (city[i].name == "Springfield") {
-            springfield.appendChild(myArticle);
-        }
+        myArticle.appendChild(myImg);
+
+        section.appendChild(myArticle);
     }
 }
+
+
+
+
+
+
